452599200e56bd05495a6f3ca93983e8133dd8c8
dasha <329191-23>
2025-02-21 15:49:21 +0300

    created class

---
+++ PurchaseManeger/Purchase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PurchaseManeger
+{
+    public enum Category
+    {
+        Продукты,
+        Техника,
+        Одежда,
+        Прочее
+    }
+    public class Purchase
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public Category Category { get; set; }
+        public DateTime Date { get; set; }
+        public Purchase(string name, decimal price, Category category, DateTime date)
+        {
+            Name = name;
+            Price = price;
+            Category = category;
+            Date = date;
+        }
+    }
+}
---
+++ PurchaseManeger/PurchaseManager.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PurchaseManeger
+{
+    public class PurchaseManager
+    {
+        public List<Purchase> Purchases { get; private set; }
+        public PurchaseManager()
+        {
+            Purchases = new List<Purchase>();
+            LoadPurchases();
+        }
+        public void AddPurchase(Purchase purchase)
+        {
+            if (purchase == null)
+            {
+                throw new ArgumentNullException(nameof(purchase));
+            }
+            Purchases.Add(purchase);
+            SavePurchases();
+        }
+        public void RemovePurchase(Purchase purchase)
+        {
+            if (purchase == null)
+            {
+                throw new ArgumentNullException(nameof(purchase));
+            }
+            Purchases.Remove(purchase);
+            SavePurchases();
+        }
+        public List<Purchase> GetPurchasesByCategory(Category category)
+        {
+            return Purchases.Where(p => p.Category == category).ToList();
+        }
+        private void SavePurchases()
+        {
+            File.WriteAllLines("purchases.txt", Purchases.Select(p =>
+            $"{p.Name}|{p.Price}|{(int)p.Category}|{p.Date.ToString("yyyy-MM-dd HH:mm:ss")}"));
+        }
+        private void LoadPurchases()
+        {
+            if (File.Exists("purchases.txt"))
+            {
+                var lines = File.ReadAllLines("purchases.txt");
+                foreach (var line in lines)
+                {
+                    var parts = line.Split('|');
+                    if (parts.Length == 4)
+                    {
+                        decimal price;
+                        if (decimal.TryParse(parts[1], out price))
+                        {
+                            Category category = (Category)Enum.Parse(typeof(Category), parts[2]);
+                            DateTime date;
+                            if (DateTime.TryParse(parts[3], out date))
+                            {
+                                Purchases.Add(new Purchase(parts[0], price, category, date));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+}
